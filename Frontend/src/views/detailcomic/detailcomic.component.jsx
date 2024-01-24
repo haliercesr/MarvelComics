9deb1deca07b488fe9cb9c4421f3ad75c26810d4
haliercesr <haliercesr@gmail.com>
2024-01-24 17:57:38 -0300

    se agrego las rutas y las views

---
+++ Frontend/src/views/detailcomic/detailcomic.component.jsx
@@ -0,0 +1,9 @@
+import style from './detailcomic.module.css'
+
+function Detailcomic (props){
+    return(<div className={style.contenedorDetailcomic}>
+    HOLA ESTAS EN DETALLE DEL COMIC
+    </div>)
+}
+
+export default Detailcomic
---
+++ Frontend/src/views/home/home.component.jsx
@@ -0,0 +1,9 @@
+import style from './home.module.css'
+
+function Home (props){
+    return(<div className={style.contenedorHome}>
+    HOLA ESTAS EN HOME
+    </div>)
+}
+
+export default Home
