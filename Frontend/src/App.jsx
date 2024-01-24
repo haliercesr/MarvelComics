b58c163d63e4a0e196db4598f91e638a06748df2
haliercesr <haliercesr@gmail.com>
2024-01-24 20:54:43 -0300

    se creo la searchbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,18 +2,19 @@
 import Home from './views/home/home.component';
 import Favourite from './views/favourite/favourite.component';
 import Detailcomic from './views/detailcomic/detailcomic.component';
+import SearchBar from './components/searchbar.component';
 //Commons imports
 import { Route, Routes, useLocation } from 'react-router-dom';
 //Styles
 import './App.css'
 
 function App() {
-  //const location=useLocation()
-  //const nav=()=>{return location.pathname.includes('/detail')? <SearchBar/>:null}
+  const location=useLocation()
+  const nav=()=>{return location.pathname.includes('/detailcomic/:id')? null:<SearchBar/>}
   
   return (
     <>
-       {/*nav()*/}
+       {nav()}
       <Routes>
         <Route path='/home' element={<Home />} />
         <Route path='/detailcomic/:id' element={<Detailcomic/>} />
---
+++ Frontend/src/components/searchbar.component.jsx
@@ -0,0 +1,21 @@
+//Styles
+import style from './searchbar.module.css';
+import imagelogo from '../images/logomarvel.png';
+import starimage from '../images/estrellavacia.png'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+function SearchBar(props) {
+
+    return (<div className={style.searchbarContainer}>
+            <img src={imagelogo} alt='logo' className={style.logoBar} />
+            <div className={style.separadorVertical}></div>
+            <button>{<i class="fa-solid fa-magnifying-glass"></i>}</button>
+            <input type="search" placeholder='Buscar' ></input>
+
+            <div className={style.starBar}><i class="fa-regular fa-star"></i></div>
+           
+        </div>);
+}
+
+export default SearchBar;
+
