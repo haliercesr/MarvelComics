c6cbde8b3dbdbccf52283152c4c02398aed4e2ab
haliercesr <haliercesr@gmail.com>
2024-01-26 13:58:46 -0300

    se crearon rutas de busqueda por id para personajes y comics

--- Backend/src/handlers/charactersRouterHandler.js
+++ Backend/src/handlers/charactersRouterHandler.js
@@ -1,4 +1,4 @@
-const {getCharacters}=require('../controllers/charactersRouter')
+const {getCharacters,getbyIdCharacter,getbyIdComics}=require('../controllers/charactersRouter')
 
 //GET | /characters
 const charactersHandler= async (req,res)=>{
@@ -12,6 +12,41 @@ const charactersHandler= async (req,res)=>{
     }
 }
 
+//GET | /characters/:id
+const characterIdHandler= async (req,res)=>{
+    const Id=req.params.id
+    try{
+    const data=await getbyIdCharacter(Id)
+    
+    if(!data.data.results[0].name) throw Error("Error al obtener el personaje")
+
+    return res.status(200).json(data.data.results[0])
+
+} catch (error) {
+    console.log(error.message)
+     return res.status(400).json({ error: error.message }) 
+    }
+}
+
+//GET | /characters/:id/comics
+const characterComicsHandler= async (req,res)=>{
+    const Id=req.params.id
+    try{
+    const data=await getbyIdComics(Id)
+    
+    if(!data.data.results[0]) throw Error("Error al obtener los comics del personaje")
+
+    return res.status(200).json(data.data.results)
+
+} catch (error) {
+    console.log(error.message)
+     return res.status(400).json({ error: error.message }) 
+    }
+}
+
 module.exports={
     charactersHandler,
+    characterIdHandler,
+    characterComicsHandler,
+
 }
