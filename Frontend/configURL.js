2087558bda4c23a9df866aeec584142b3b47d5a5
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-01-29 02:32:08 -0300

    Update configURL.js

--- Frontend/configURL.js
+++ Frontend/configURL.js
@@ -1,4 +1,4 @@
-export const URLfrontend="http://localhost:5173"
-export const URLSERVER = 'http://localhost:3001'
-//export const URLfrontend='';
-//export const URLSERVER='';
+//export const URLfrontend="http://localhost:5173"
+//export const URLSERVER = 'http://localhost:3001'
+export const URLfrontend='https://marvel-comics-vqta.vercel.app';
+export const URLSERVER='https://backendmarvel.onrender.com';
